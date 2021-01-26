01c4d37d55c1b88c466949f946625bb34955e239
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2021-01-25 17:16:13 -0700

    Add files via upload

--- HW-2 Files/readme.md
+++ HW-2 Files/readme.md
@@ -1,1 +1,5 @@
+This week was a very relaxing entry into JavaScript, and I actually enjoyed doing it.
 
+My progress was pretty fast, and was free of any issues I could find. I follow a LOT of tech Youtube
+channels, and  that was the vast majority of my websites. Games I play all over the place, but 
+I sstuck with my 3 faves. As for artists
---
+++ HW-2 Files/script.js
@@ -0,0 +1,24 @@
+// If you open the web console.
+// My 3 Favorite websites
+console.log("3 Favorite Websites")
+// insert a blank line for easier readability
+console.log("..................")
+console.log("https://www.youtube.com/user/Jayztwocents");
+console.log("https://www.Amazon.com");
+console.log("https://www.youtube.com/user/LinusTechTips");
+// insert another blank line for readability
+console.log("--- ----- ------ ---- ---- ---- ---- ---- --- ----");
+console.log("3 Favorite Games");
+//insert blank line again...
+console.log("--- ---- --- --- --- --- --- --- --- --- --- ---");
+console.log("Doom Eternal (Sequel to Doom 2016");
+console.log('Doom (2016)');
+console.log("Final Fantasy XIV, MMORPG");
+console.log("Shadow of the Tomb Raider");
+//insert blank line yet again...
+console.log("3 Favorite Artists");
+// insert blank line....
+console.log("--- --- --- --- --- --- ---");
+console.log("Metallica");
+console.log("Lilla Bee Cosplay");
+console.log("7bpictures");
